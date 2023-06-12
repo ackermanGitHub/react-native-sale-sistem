8adcb226fb914fb917c2e4b6d435c92e1d84df4a
ackerman <julio.sergio2709@gmail.com>
2023-06-12 01:20:01 -0400

    🖌️➕➖: tabBarLabel and tabBarIcon removed

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
+import { Pressable, useColorScheme, Text } from 'react-native';
 
 import Colors from '../../constants/Colors';
 
@@ -27,16 +27,25 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Primary View',
+          title: 'Cajero',
           headerTintColor: "#111111",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={"#111111"} />,
+          /* tabBarIcon: ({ color }) => <TabBarIcon name="code" color={"#111111"} />, */
+          tabBarLabel: ({ color, children }) => (
+            <Text style={{
+              color: color,
+              fontSize: 20,
+            }}
+            >
+              {children}
+            </Text>
+          ),
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
                 {({ pressed }) => (
                   <FontAwesome
                     name="info-circle"
-                    size={25}
+                    size={30}
                     /* color={Colors[colorScheme ?? 'light'].text} */
                     color={"#111111"}
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
@@ -50,9 +59,34 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Secondary',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={"#111111"} />,
+          title: 'Órdenes',
+          /* tabBarIcon: ({ color }) => <TabBarIcon name="code" color={"#111111"} />, */
+          tabBarLabel: ({ color, children }) => (
+            <Text style={{
+              color: color,
+              fontSize: 20,
+            }}
+            >
+              {children}
+            </Text>
+          ),
+          headerRight: () => (
+            <Link href="/modal" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="info-circle"
+                    size={30}
+                    /* color={Colors[colorScheme ?? 'light'].text} */
+                    color={"#111111"}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
         }}
+
       />
     </Tabs>
   );
