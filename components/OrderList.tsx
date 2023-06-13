a0d28be8e5946c034e9ce983ea33fef5715de5b1
ackerman <julio.sergio2709@gmail.com>
2023-06-13 03:14:36 -0400

    ⚙️: OrderList component ws protocol fix

--- components/OrderList.tsx
+++ components/OrderList.tsx
@@ -1,13 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
 const OrderList = () => {
     const [orders, setOrders] = useState<string[]>([]);
+    const [ws, setWs] = useState<WebSocket | null>(null);
+
+    const handleWebSocketMessage = (event: MessageEvent) => {
+        console.log(event.data)
+        setOrders((prevOrders) => [...prevOrders, event.data]);
+    };
     useEffect(() => {
-        const ws = new WebSocket('wss://websocketscustomdomain.up.railway.app', 'orderReceiver');
-        ws.addEventListener('message', (event) => {
-            setOrders((prevOrders) => [...prevOrders, event.data]);
+        const ws = new WebSocket("ws://192.168.49.191:3333", 'ordersReciever');
+        setWs(ws);
+
+        ws.addEventListener("open", (event) => {
+            console.log('%c (ordersSender) Connection opened', 'background: orange; color: black;', event);
         });
+
+        ws.addEventListener('message', handleWebSocketMessage);
+
+        ws.addEventListener('close', (event) => {
+            console.log('%c (ordersSender) Connection closed', 'background: orange; color: black;', event);
+        });
+
+        ws.addEventListener('error', (error) => {
+            console.log('%c (ordersSender) WebSocket error', 'background: red; color: black;', error);
+        });
+
         return () => {
+            ws.removeEventListener("message", handleWebSocketMessage);
             ws.close();
         };
     }, []);
@@ -23,6 +43,11 @@ const OrderList = () => {
                 renderItem={renderItem}
                 keyExtractor={(item, index) => index.toString()}
             />
+            <TouchableOpacity style={styles.button} onPress={() => {
+                console.log(orders)
+            }}>
+                <Text style={styles.orderText}>1</Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -41,6 +66,14 @@ const styles = StyleSheet.create({
     },
     orderText: {
         fontSize: 16,
+    },
+    button: {
+        margin: 5,
+        width: '30%',
+        backgroundColor: '#ddd',
+        borderRadius: 5,
+        textAlign: 'center',
+        justifyContent: 'center',
     },
 });
 export default OrderList;
