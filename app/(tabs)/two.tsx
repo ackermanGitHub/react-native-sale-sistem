c61d961f6c66cb46a4e342f439ee92e84136ebbc
ackerman <julio.sergio2709@gmail.com>
2023-06-14 03:45:50 -0400

    🖌️:  background to #E5E5CB

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -4,7 +4,7 @@ import tw from 'twrnc';
 
 export default function TabTwoScreen() {
   return (
-    <View style={tw`flex-1 items-center justify-center`}>
+    <View style={tw`flex-1 items-center justify-center bg-[#E5E5CB]`}>
       <View style={tw`my-7 h-[1px] w-[4/5]`} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <OrderList />
       <View style={tw`my-7 h-[1px] w-[4/5]`} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
