c610212eaed0e598740ba741de31b6601870b8b3
ackerman <julio.sergio2709@gmail.com>
2023-06-14 00:44:30 -0400

    ➕: Making shopping screen scrollable on movile

--- components/OrderScreen.tsx
+++ components/OrderScreen.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import tw from 'twrnc';
 
 const OrderScreen = ({ order = 'Order' }) => {
     return (
         <View style={tw`justify-center items-center w-full h-[15%]`}>
             <View style={tw`flex-row justify-center items-center w-4/5 h-4/5 bg-[#fff] rounded-xl shadow-lg`}>
-                <Text style={tw`p-4 text-2xl font-normal text-left`}>{order}</Text>
+                <ScrollView horizontal={true}>
+                    <Text style={tw`p-4 text-2xl font-normal text-left`}>{order}</Text>
+                </ScrollView>
             </View>
         </View>
     );
 };
-
 export default OrderScreen;
