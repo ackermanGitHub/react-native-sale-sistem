43e654bbc34d2ad5ba1b27f754f507aca2f398f9
ackerman <julio.sergio2709@gmail.com>
2023-06-15 03:14:00 -0400

    🖌️: modal to tailwind styles and custom navbar

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,36 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+
+import { useEffect } from 'react';
+import * as NavigationBar from 'expo-navigation-bar';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import tw from 'twrnc';
 
 export default function ModalScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
 
-      <StatusBar backgroundColor='#999999' style={'dark'} />
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      {/* <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} /> */}
+  useEffect(() => {
+    NavigationBar.setBackgroundColorAsync("white");
+
+    return () => {
+      NavigationBar.setBackgroundColorAsync("#E5E5CB");
+
+    }
+
+  }, [])
+
+  return (
+    <View style={tw``}>
+      <StatusBar backgroundColor='white' style={'dark'} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
