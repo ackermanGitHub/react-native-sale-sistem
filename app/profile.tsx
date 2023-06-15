708e6999235228b5fd5e50ada64b636b88a3ccc5
ackerman <julio.sergio2709@gmail.com>
2023-06-15 03:14:19 -0400

    ➕: profile route

---
+++ app/profile.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+
+import { useEffect } from 'react';
+import * as NavigationBar from 'expo-navigation-bar';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import tw from 'twrnc';
+
+export default function ModalScreen() {
+
+    useEffect(() => {
+        NavigationBar.setBackgroundColorAsync("white");
+
+        return () => {
+            NavigationBar.setBackgroundColorAsync("#E5E5CB");
+
+        }
+
+    }, [])
+
+    return (
+        <View style={tw``}>
+            <StatusBar backgroundColor='white' style={'dark'} />
+        </View>
+    );
+}
