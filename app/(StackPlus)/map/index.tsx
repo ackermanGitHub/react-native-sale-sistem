e1fd8b6d21629968f6b943b9514a3faaf3bf2de6
ackerman <julio.sergio2709@gmail.com>
2023-06-18 13:14:24 -0400

    this was lefted

--- app/(StackPlus)/map/index.tsx
+++ app/(StackPlus)/map/index.tsx
@@ -3,7 +3,7 @@ import { View } from '../../../components/Themed';
 import tw from 'twrnc';
 import MapViewComponent from '../../../components/MapView';
 
-export default function App() {
+export default function MapViewRoute() {
     return (
         <View style={tw`w-full h-full`} >
             <MapViewComponent />
