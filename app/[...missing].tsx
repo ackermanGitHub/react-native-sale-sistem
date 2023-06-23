0f55e023a97340e7b3ef92e737a9d8eccaadc5f7
ackerman <julio.sergio2709@gmail.com>
2023-06-23 12:53:17 -0400

    ➕; pathname info to [...missing] routes

--- app/[...missing].tsx
+++ app/[...missing].tsx
@@ -1,9 +1,11 @@
 import { Link, Stack } from 'expo-router';
 import tw from '../components/utils/tailwind';
+import { usePathname } from 'expo-router';
 
 import { Text, View } from '../components/Themed';
 
 export default function NotFoundScreen() {
+  const pathname = usePathname();
   return (
     <>
       <Stack.Screen options={{
@@ -11,7 +13,7 @@ export default function NotFoundScreen() {
       }} />
       <View style={tw`flex-1 justify-center items-center p-5`}>
         <Text style={tw`text-xl font-bold`}>This screen doesn't exist.</Text>
-
+        <Text style={tw`text-lg text-red-500 mt-2`}>Missing: {pathname}</Text>
         <Link href="/" style={tw`mt-4 py-4`}>
           <Text style={tw`text-sm text-[#2e78b7]`}>Go to home screen!</Text>
         </Link>
